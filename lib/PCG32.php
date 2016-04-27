fe7d5d2a712257cc808b58e145eb09eb3e699e0c
Leigh <leight@gmail.com>
2016-04-27 10:16:15 +0100

    Rename vars.

--- lib/PCG32.php
+++ lib/PCG32.php
@@ -4,37 +4,39 @@ namespace PCG;
 
 class PCG32
 {
-    private $rng = [
-        -8846114313915602277, // 0x853c49e6748fea9b
-        -2720673578348880933  // 0xda3e39cb94b95bdb
-    ];
+    private $state = -8846114313915602277; // 0x853c49e6748fea9b
+    private $sequence = -2720673578348880933; // 0xda3e39cb94b95bdb
 
     function srandom(int $state, int $sequence)
     {
-        $this->rng[0] = 0;
-        $this->rng[1] = ($sequence << 1) | 1;
+        $this->state = 0;
+        $this->sequence = ($sequence << 1) | 1;
         $this->random();
-        $this->rng[0] += $state;
+        $this->state += $state;
         $this->random();
     }
 
     function random(): int
     {
-        $oldState = $this->rng[0];
+        $previousState = $this->state;
 
         // This is 64-bit unsigned int multiplication equivalent to:
-        // $this->rng[0] = $oldState * 6364136223846793005 + $this->rng[1];
-        $old0 = $oldState & 0xffffffff;
-        $old1 = ($oldState >> 32) & 0xffffffff;
+        // $this->state = $previousState * 6364136223846793005 + $this->sequence;
+        // 0x4c957f2d * 0xffffffff, 0x5851f42d * 0xffffffff
+        // are both less than 2**63, so it is safe to use 32 bit operands
 
-        $carry = (0x4c957f2d * $old0) + $this->rng[1];
-        $old1 = ((0x4c957f2d * $old1) & 0xffffffff) +
-                ((0x5851f42d * $old0) & 0xffffffff) +
-                (($carry >> 32) & 0xffffffff);
+        $prev0 = $previousState & 0xffffffff;
+        $prev1 = ($previousState >> 32) & 0xffffffff;
 
-        $this->rng[0] = ($old1 << 32) | ($carry & 0xffffffff);
+        $carry = (0x4c957f2d * $prev0) + ($this->sequence & 0xffffffff);
+        $prev1 = ((0x4c957f2d * $prev1) & 0xffffffff) +
+                ((0x5851f42d * $prev0) & 0xffffffff) +
+                (($carry >> 32) & 0xffffffff) +
+                (($this->sequence >> 32) & 0xffffffff);
 
-        return $this->xsh_rr_64_32($oldState);
+        $this->state = ($prev1 << 32) | ($carry & 0xffffffff);
+
+        return $this->xsh_rr_64_32($previousState);
     }
 
     private function ror32(int $value, int $amount): int
